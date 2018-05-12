5869bdda613aef2df2acbfccd8848775872f0c28
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-11 20:56:07 -0400

    commit additional changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,53 @@
 //Friend Finder Survey (DATA)
 //=================================================================
 var surveyData = [{
-    name: "Mark Sanchez",
-    photo: "http://www.espn.com/nfl/player/_/id/12482/mark-sanchez",
-    scores: [
-        "5",
-        "1",
-        "4",
-        "4", 
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-    ]
-},
-{
-    name: "Beyonce Knowles",
-    photo: "https://nyppagesix.files.wordpress.com/2018/05/beyonce-solange.jpg?quality=90&strip=all&w=618&h=410&crop=1",
-    scores: [
-        "1",
-        "1",
-        "4",
-        "4", 
-        "5",
-        "5",
-        "3",
-        "3",
-        "2",
-        "1"
-    ]
-},
-{
-    name: "Hillary Clinton",
-    photo: "http://thepoliticalcarnival.net/2012/12/15/hillary-clinton-faints-has-concussion/hillary-rodham-clinton/",
-    scores: [
-        "1",
-        "1",
-        "4",
-        "4", 
-        "5",
-        "5",
-        "3",
-        "3",
-        "2",
-        "1"
-    ]
-},
+        name: "Mark Sanchez",
+        photo: "http://www.espn.com/nfl/player/_/id/12482/mark-sanchez",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Beyonce Knowles",
+        photo: "https://nyppagesix.files.wordpress.com/2018/05/beyonce-solange.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+        scores: [
+            "1",
+            "1",
+            "4",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
+    {
+        name: "Hillary Clinton",
+        photo: "http://thepoliticalcarnival.net/2012/12/15/hillary-clinton-faints-has-concussion/hillary-rodham-clinton/",
+        scores: [
+            "1",
+            "1",
+            "4",
+            "4",
+            "5",
+            "5",
+            "3",
+            "3",
+            "2",
+            "1"
+        ]
+    },
 ]
 
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 //Dependencies 
 //=================================================================
 var express = require("express");
-var bp = require("body-parser");
+var bodyParser = require("body-parser");
 var path = require("path");
 
 //Sets Up the Express App
 //=================================================================
 var app = express();
-var PORT = 3000;
+var appPORT = process.env.PORT || 8080;
+
+//Enables Express to serve static files.
+app.use(express.static('app/public'))
 
 //Sets Up the Express app to handle data parsing
 //=================================================================
-app.use(bodyParser.urlendcoded({
+app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(bodyParser.json());
+
+//Require additional routing files for API Routes and HMTL Routes
+require('.app/routing/apiRoutes.js')(app);
+require('.app/routing/htmlRoutes.js')(app);
 
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(appPORT, function() {
+    console.log("App listening on PORT " + appPORT);
 });
