690d0b71996134b9a682799d17b7e565cf503480
Reinaldo Llano <llanoreinaldo@gmail.com>
2018-05-11 20:21:45 -0400

    update to friends js and apiRoutes files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,52 @@
 //Friend Finder Survey (DATA)
 //=================================================================
 var surveyData = [{
-    surveyQuestion: "Res",
-    name: "name",
-    photo: "photo",
+    name: "Mark Sanchez",
+    photo: "http://www.espn.com/nfl/player/_/id/12482/mark-sanchez",
     scores: [
-        "q1",
-        "q2",
-        "q3",
-        "q4", 
-        "q5",
-        "q6",
-        "q7",
-        "q8",
-        "q9",
-        "q10"
+        "5",
+        "1",
+        "4",
+        "4", 
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
     ]
-}]
+},
+{
+    name: "Beyonce Knowles",
+    photo: "https://nyppagesix.files.wordpress.com/2018/05/beyonce-solange.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+    scores: [
+        "1",
+        "1",
+        "4",
+        "4", 
+        "5",
+        "5",
+        "3",
+        "3",
+        "2",
+        "1"
+    ]
+},
+{
+    name: "Hillary Clinton",
+    photo: "http://thepoliticalcarnival.net/2012/12/15/hillary-clinton-faints-has-concussion/hillary-rodham-clinton/",
+    scores: [
+        "1",
+        "1",
+        "4",
+        "4", 
+        "5",
+        "5",
+        "3",
+        "3",
+        "2",
+        "1"
+    ]
+},
+]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,20 +1,19 @@
-
 // Routes
 // =============================================================
 
 // Displays a single character, or returns false
-app.get("/api/friends", function(req, res) {
+app.get("/api/friends", function (req, res) {
     return res.json(surveyData);
 });
 
-app.post("/api/friends", function(req, res) {
-var newSurvey = req.body; 
+app.post("/api/friends", function (req, res) {
+    var newSurvey = req.body;
 
-newSurvey.surveyQuestion = newSurvey.name.replace(/\s+/g, "").toLowerCase();
+    newSurvey.name = newSurvey.name.replace(/\s+/g, "").toLowerCase();
 
-console.log(newSurvey);
+    console.log(newSurvey);
 
-surveyData.push(newSurvey);
+    surveyData.push(newSurvey);
 
-res.json(newSurvey);
+    res.json(newSurvey);
 });
